bd3fcebc798632296253015cab2e0620a263e4b8
mayuran-kuhathasan <emailmayuran@gmail.com>
2021-02-09 22:14:44 +0000

    2.0 itelline

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
 class Product < ApplicationRecord
+	has_many :line_items
+	before_destroy :ensure_not_refrenced_by_any_line_item
+
+	validates :title, :description, :image_url,  presence: true
+	validates :price, numericality: { greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+	validates :image_url, allow_blank: true, format: {with: %r{\.(gif|jpg|png)\z}i, message: 'must be GIF, JPG, OR PING image'}
+
+
+
+	private
+		def ensure_not_refrenced_by_any_line_item
+			unless line_items.empty?
+				errors.add(:base, 'Line item present')
+				throw :abort
+			end
+
+		end
+
 end
+
